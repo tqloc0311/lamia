a3ab45bdb2a0ce323aa41bdace83d11522aac389
loc.tran <tqloc0311@gmail.com>
2024-12-12 00:35:28 +0700

    toggle password visibillity

--- src/components/auth/auth-text-input.tsx
+++ src/components/auth/auth-text-input.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { combineStyles } from '../../utils/helpers';
 import TextInput, { CTextInputProps } from '../shared/custom-text-input';
 import { Box } from '@lamia/utils/theme';
@@ -8,10 +8,39 @@ interface AuthTextInputProps extends CTextInputProps {}
 
 const AuthTextInput = (props: AuthTextInputProps) => {
   const inputStyle = combineStyles(props.style, styles.input);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const secureTextEntry = props.secureTextEntry && !isPasswordVisible;
 
   return (
-    <Box borderBottomColor="white" borderBottomWidth={1} py="2" my="3">
-      <TextInput placeholderTextColor="white" {...props} style={inputStyle} />
+    <Box
+      borderBottomColor="white"
+      borderBottomWidth={1}
+      py="2"
+      my="3"
+      flexDirection="row"
+      alignItems="center">
+      <Box flex={1}>
+        <TextInput
+          placeholderTextColor="white"
+          {...props}
+          style={inputStyle}
+          secureTextEntry={secureTextEntry}
+        />
+      </Box>
+      <TouchableOpacity onPress={() => setIsPasswordVisible(prev => !prev)}>
+        {props.secureTextEntry && isPasswordVisible && (
+          <Image
+            source={require('../../assets/images/eye_white.png')}
+            style={styles.icon}
+          />
+        )}
+        {props.secureTextEntry && !isPasswordVisible && (
+          <Image
+            source={require('../../assets/images/closed_eye_white.png')}
+            style={styles.icon}
+          />
+        )}
+      </TouchableOpacity>
     </Box>
   );
 };
@@ -22,4 +51,5 @@ const styles = StyleSheet.create({
   input: {
     color: 'white',
   },
+  icon: { width: 20, height: 20 },
 });
